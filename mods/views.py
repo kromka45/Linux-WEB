f9ef9566393de1316d8e0f15ad78fde86541d8ab
kromka <modern1995.hz@gmail.com>
2025-12-06 02:10:05 +0100

    0.0.1 ALFA rework (REMOVED FILES) TEST

--- mods/views.py
+++ mods/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from .models import Mody
 from rest_framework import viewsets
 from mods.serializer import ModySerializer
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import json
 
 class ModyViewSet(viewsets.ModelViewSet):
     queryset = Mody.objects.all()
     serializer_class = ModySerializer
+    posts = Mody.objects.all()
+    serializer1 = ModySerializer(posts, many=True)
+    data =serializer1.data
+    with open("wynik.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+
+
 
 
 
